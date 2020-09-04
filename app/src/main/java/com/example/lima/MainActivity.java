d46ae4a076920030aefc9844b49de5195009e59a
Eleanor <eleanor.straight@my.wheaton.edu>
2020-09-04 13:14:00 -0700

    Added some contents to VF00

--- app/src/main/java/com/example/lima/MainActivity.java
+++ app/src/main/java/com/example/lima/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.lima;
 
-/**** Added by me *****/
+///// Added by me //////
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-/**********************/
+/////////////////////////
 import android.view.Menu;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
--- app/src/main/java/com/example/lima/VF0Activity.java
+++ app/src/main/java/com/example/lima/VF0Activity.java
@@ -2,15 +2,29 @@ package com.example.lima;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 /* VF1 - ¿Qué es la violencia? */
 public class VF0Activity extends AppCompatActivity {
-
+    Button vfm01, vfm02, vfm03, vfm04, vfm05, vfm06;
+    TextView vf01, vf02, vf03, vf04, vf05, vf06;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_v_f_0);
+
+        vfm01 = new Button(this);
+        vf01=new TextView(this);
+        //vf01.setOnClickListener(this);
+
+        vfm01 = (Button)findViewById(R.id.VFMenu01);
+        vf01=(TextView)findViewById(R.id.VF01);
+        //vf01.setVisibility(TextView.INVISIBLE);
+
+
     }
 
     @Override
